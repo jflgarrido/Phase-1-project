5960c362721c9159f0cea7344e6d22b426d51ba5
Julius Garrido <jflgarrido@gmail.com>
2022-01-05 21:47:09 -0700

    Progress

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 
 //Node Getters
 const mainDiv = () => document.getElementById('main');
+const homePageLink = () => document.getElementById('home-link');
+const planetsLink = () => document.getElementById('planets-link')
+const peopleLink = () => document.getElementById('people-link')
 
 //Templates
 const homePageTemplate = () => {
@@ -12,15 +15,45 @@ const homePageTemplate = () => {
     `
 }
 const planetsTemplate = () => {
-    console.log('planets!')
+    return `
+    <h1 class="center-align">Planets!</h1>
+    `
 }
 const peopleTemplate = () => {
-    console.log('people!')
+    return `
+    <h1 class="center-align">People!</h1>
+    `
 }
 
 //Renderers
 const renderHomePage = () => {
     mainDiv().innerHTML = homePageTemplate();
+}
+const renderPlanetsPage = () => {
+    mainDiv().innerHTML = planetsTemplate();
+}
+const renderPeoplePage = () => {
+    mainDiv().innerHTML = peopleTemplate();
+}
+
+//Events
+const homePageLinkEvent = () => {
+    homePageLink().addEventListener('click', (e) => {
+        e.preventDefault();
+        renderHomePage();
+    })
+}
+const planetsLinkEvent = () => {
+    planetsLink().addEventListener('click', (e) => {
+        e.preventDefault();
+        renderPlanetsPage();
+    })
+}
+const peopleLinkEvent = () => {
+    peopleLink().addEventListener('click', (e) => {
+        e.preventDefault();
+        renderPeoplePage();
+    })
 }
 
 //*****
@@ -30,4 +63,7 @@ const renderHomePage = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     renderHomePage();
+    homePageLinkEvent();
+    planetsLinkEvent();
+    peopleLinkEvent();
 })
